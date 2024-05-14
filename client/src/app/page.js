494265ecc2584219248451800fdb40dbf4e25bbc
Faizank7 <77374778+Faizank7@users.noreply.github.com>
2024-05-15 00:31:46 +0500

    All done

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,5 +1,6 @@
 
 import Feature from "@/components/Feature";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
@@ -10,6 +11,7 @@ export default function Home() {
       <Navbar/>
       <Hero/>
      <Feature/>
+     <Footer/>
     </main>
   );
 }
