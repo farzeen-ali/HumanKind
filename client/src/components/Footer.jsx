e1ec893baa2a4d1bce3e397e8a4398bd288f17c0
Faizank7 <77374778+Faizank7@users.noreply.github.com>
2024-05-15 00:22:34 +0500

    Footer done

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+  
+        <footer className="bg-gray-800 text-gray-300 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div>
+          <p>&copy; 2024 Your Company. All rights reserved.</p>
+        </div>
+        <div>
+          <a href="#" className="mr-4 hover:text-white">Home</a>
+          <a href="#" className="mr-4 hover:text-white">About</a>
+          <a href="#" className="hover:text-white">Contact</a>
+        </div>
+      </div>
+    </footer>
+    
+  )
+}
+
+export default Footer
